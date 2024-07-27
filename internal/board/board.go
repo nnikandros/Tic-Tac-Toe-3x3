c8c297811de8e1949ebd72ed93322c443672f786
Nikitas Nikandros <nikitas@bycape.io>
2024-07-27 22:19:04 +0300

    parametrize legnth , width

--- internal/board/board.go
+++ internal/board/board.go
@@ -1,8 +1,21 @@
 package board
 
-type Board [3][3]int8
+const Length = 3
+const Width = 3
+
+type Board [Length][Width]int8
 
 type Coordinates struct {
 	X uint8
 	Y uint8
+}
+
+func (b Board) HasBeenset(p Coordinates) (int8, bool) {
+	v := b[p.X][p.Y]
+	if v == 0 {
+		return 0, false
+	}
+
+	return v, true
+
 }
