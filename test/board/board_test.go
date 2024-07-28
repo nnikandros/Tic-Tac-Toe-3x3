ef33e7f9cb8316b13062ad272a6b3c1ed5375e7d
Nikitas Nikandros <nikitas@bycape.io>
2024-07-28 13:05:32 +0300

    add board2

--- test/board/board_test.go
+++ test/board/board_test.go
@@ -8,8 +8,29 @@ import (
 
 func TestBoard(t *testing.T) {
 
-	b := board.Board{}
+	// b := board.Board{}
+
+	// fmt.Println(b)
+
+	x := map[int]string{0: "kaaka", 1: "papiko"}
+
+	if v, ok := x[3]; !ok {
+		fmt.Println("NOT OK")
+		fmt.Printf("vaue of string is %s", v)
+	}
+
+}
+
+func TestBoard2(t *testing.T) {
+
+	b := board.Board2{}
 
 	fmt.Println(b)
+
+	v, ok := b.Value(board.Coordinates{0, 0})
+
+	fmt.Println(v)
+
+	fmt.Println(ok)
 
 }
