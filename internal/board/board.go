ef3e231cb1b5d2edbb5482fa4765cb4f57b3ae35
Nikitas Nikandros <nikitas@bycape.io>
2024-07-31 10:20:49 +0300

    add code

--- internal/board/board.go
+++ internal/board/board.go
@@ -3,6 +3,9 @@ package board
 const Length = 3
 const Width = 3
 
+var PlayerXMark = 1
+var PlayerOMark = 0
+
 type Board [Length][Width]int8
 
 func (b Board) HasBeenset(p Coordinates) (int8, bool) {
@@ -26,5 +29,13 @@ func (b Board2) Value(p Coordinates) (int, bool) {
 	}
 
 	return *v, true
+
+}
+
+func (b *Board2) Set(p Coordinates) {
+	if value, ok := b.Value(p); ok {
+	}
+
+	
 
 }
