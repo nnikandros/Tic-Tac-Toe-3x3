2b4c452835e3b3233a5eaec409fb9ebed5ac355e
Nikitas Nikandros <Nikitas.NIKANDROS@ext.ec.europa.eu>
2024-08-09 07:51:57 +0000

    add server and route

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,1 +1,15 @@
 package server
+
+import "net/http"
+
+func (s *Server) RegisterRoutes() http.Handler {
+	mux := http.NewServeMux()
+
+	// mux.HandleFunc("GET /", s.HelloWorldHandler)
+	return mux
+
+}
+
+func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
+
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,28 @@
 package server
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+type Server struct {
+	port int
+}
+
+func NewServer() *http.Server {
+	port, _ := strconv.Atoi("3000")
+	NewServer := &Server{port: port}
+
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", NewServer.port),
+		Handler:      NewServer.RegisterRoutes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	return server
+
+}
